a7631c117480d59deb2708b8f9550341f1d38af1
admin <admin@DESKTOP-92V2IOK>
2024-10-14 21:42:04 +0900

    화면단

---
+++ cds_spring_repo/src/main/java/com/human/cds/api/ApiExplorerDetail3.java
@@ -0,0 +1,70 @@
+package com.human.cds.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.human.cds.vo.AcommointroVO;
+import com.human.cds.vo.CourseInfoDTO3;
+
+
+public class ApiExplorerDetail3 {
+
+    public static AcommointroVO getDetailByContentId(String serviceKey, String contentId, String contentTypeId)
+            throws IOException, URISyntaxException {
+        
+        // API 요청 URL 설정
+//    	String srcUrl = "https://apis.data.go.kr/B551011/KorService1/detailIntro1";
+        String srcUrl = "https://apis.data.go.kr/B551011/KorService1/detailInfo1";
+        StringBuilder urlBuilder = new StringBuilder(srcUrl);
+        
+        // 필수 파라미터 추가
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey);
+        urlBuilder.append("&" + URLEncoder.encode("contentId", "UTF-8") + "=" + URLEncoder.encode(contentId, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("contentTypeId", "UTF-8") + "=" + URLEncoder.encode(contentTypeId, "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("MobileOS", "UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8")); // 운영체제 예시 (ETC)
+        urlBuilder.append("&" + URLEncoder.encode("MobileApp", "UTF-8") + "=" + URLEncoder.encode("MyApp", "UTF-8")); // 앱 이름
+        urlBuilder.append("&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); // 응답 형식 JSON
+
+        // URL 생성
+        URL url = new URI(urlBuilder.toString()).toURL();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        conn.setRequestProperty("Accept", "application/json");
+
+        // 응답 처리
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+//        // 응답을 CourseInfoDTO2로 변환
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        AcommointroVO data = objectMapper.readValue(sb.toString(), AcommointroVO.class);
+        
+        String jsonResponse = sb.toString().replace("\"items\": \"\"", "\"items\": null");
+
+        // 응답을 AcommointroVO로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        AcommointroVO data = objectMapper.readValue(jsonResponse, AcommointroVO.class);
+
+        return data;
+    }
+}
---
+++ cds_spring_repo/src/main/java/com/human/cds/vo/AcommoImgVO.java
@@ -0,0 +1,30 @@
+package com.human.cds.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AcommoImgVO {
+        private String content_id;       // 콘텐츠 ID
+        private String content_type_id;   // 콘텐츠 타입 ID
+        private String first_image;
+        private String area_code;
+        private String roomtitle;
+        private String roomcode;
+        private String roomoffseasonminfee1;
+        private String roomimg1;
+        private String roomimg1alt;
+        private String roomimg2;
+        private String roomimg2alt;
+        private String roomimg3;
+        private String roomimg3alt;
+        private String roomimg4;
+        private String roomimg4alt;
+        private String roomimg5;
+        private String roomimg5alt;
+        private String cnt;
+        private String title;
+        
+
+}
